6043317c27549adcb3e0ed1d23a9bee49b9b8edd
ningSP4 <ningyuan.sg@gmail.com>
2016-10-24 04:49:29 +0800

    Added static tail!

--- main.py
+++ main.py
@@ -6,17 +6,32 @@
 import pygame, sys
 from pygame.locals import *
 
-FPS = 8
+FPS = 8  # set 1 for debugging mode, 8 for normal
 WINDOW_WIDTH = 360
 WINDOW_HEIGHT = 480
 WINDOW_RES = (WINDOW_WIDTH, WINDOW_HEIGHT)
 fpsClock = pygame.time.Clock()
+DISPLAYSURF = pygame.display.set_mode(WINDOW_RES)
 # COLOURS  R :  G :  B
 WHITE  = (255, 255, 255)
 GREY   = (100, 100, 100)
 BLACK  = (  0,   0,   0)
 ORANGE = (255, 128,   0)
 UP = 'up'; DOWN = 'down'; LEFT = 'left'; RIGHT = 'right'
+
+# TAIL
+class Tail:
+    SIZE = 20
+    history = [
+            pygame.Rect(WINDOW_WIDTH/2 - 40, WINDOW_HEIGHT/2, SIZE, SIZE),
+            pygame.Rect(WINDOW_WIDTH/2 - 20, WINDOW_HEIGHT/2, SIZE, SIZE)
+            ]
+    score = 0
+
+    def draw(self):
+        for each in self.history:
+            pygame.draw.rect(DISPLAYSURF, WHITE, each)
+tail = Tail()
 
 # SNAKE
 class Snake:
@@ -28,23 +43,32 @@ class Snake:
     collide = False # turns True in loop if collide with border, or tail
     # directional
     direction = RIGHT  # start right
+
+    def update(self):
+        self.head = pygame.Rect(self.x, self.y, self.SIZE, self.SIZE)
 snake = Snake()
 
-def main(snake):
+def main(snake, tail):
     pygame.init()
-    DISPLAYSURF = pygame.display.set_mode(WINDOW_RES)
     pygame.display.set_caption("21st Century Snakedown")
 
     while True:
         DISPLAYSURF.fill(BLACK)
         pygame.draw.rect(DISPLAYSURF, WHITE, snake.head)
+        tail.draw()
+
+        if len(tail.history) == tail.score + 2:
+            tail.history = tail.history[1:]
+        tail.history.append(snake.head)
 
         for event in pygame.event.get():
             # exit
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            # change directions TODO: add wasd controls
+            # change directions
+            # TODO: add wasd controls
+            # TODO: snake can still turn 180 with two rapid taps
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP and snake.direction != DOWN:
                     snake.direction = UP
@@ -57,20 +81,17 @@ def main(snake):
 
         if snake.direction == UP:
             snake.y -= snake.SIZE
-            snake.head.top = snake.y
         elif snake.direction == DOWN:
             snake.y += snake.SIZE
-            snake.head.top = snake.y
         elif snake.direction == RIGHT:
             snake.x += snake.SIZE
-            snake.head.left = snake.x
         elif snake.direction == LEFT:
             snake.x -= snake.SIZE
-            snake.head.left = snake.x
+        snake.update()
 
         pygame.display.update()
         fpsClock.tick(FPS)
 
 
 while __name__ == '__main__':
-    main(snake)
+    main(snake, tail)
