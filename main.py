c8a61455083c395d12a139eadd48c18255e4e512
ningSP4 <ningyuan.sg@gmail.com>
2016-10-24 01:22:08 +0800

    Fixed UnboundLocalError issue by creating Snake class & object. TODO: map movement wasd and arrow keys, make snake controllable.

--- main.py
+++ main.py
@@ -7,50 +7,59 @@ import pygame, sys
 from pygame.locals import *
 
 FPS = 3
-fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 360
 WINDOW_HEIGHT = 480
 WINDOW_RES = (WINDOW_WIDTH, WINDOW_HEIGHT)
+fpsClock = pygame.time.Clock()
 # COLOURS  R :  G :  B
 WHITE  = (255, 255, 255)
 GREY   = (100, 100, 100)
 BLACK  = (  0,   0,   0)
 ORANGE = (255, 128,   0)
-# SNAKE
-snakeSIZE = 20
-snakex = WINDOW_WIDTH / 2  # start x
-snakey = WINDOW_HEIGHT / 2  # start y
-snakeHead = pygame.Rect(snakex, snakey, snakeSIZE, snakeSIZE)
-snakeCollide = False  # turns True in loop if collide with border, or tail
-UP = 'up'; DOWN = 'down'; LEFT = 'left'; RIGHT = 'right'  # snake directions
-snakeDirection = RIGHT  # start right
+UP = 'up'; DOWN = 'down'; LEFT = 'left'; RIGHT = 'right'
 
-def main():
+# SNAKE
+class Snake:
+    # spatial
+    SIZE = 20
+    x = WINDOW_WIDTH / 2  # start x
+    y = WINDOW_HEIGHT / 2  # start y
+    head = pygame.Rect(x, y, SIZE, SIZE)  #pygame Rect Obj
+    collide = False # turns True in loop if collide with border, or tail
+    # directional
+    direction = RIGHT  # start right
+snake = Snake()
+
+def main(snake):
     pygame.init()
     DISPLAYSURF = pygame.display.set_mode(WINDOW_RES)
     pygame.display.set_caption("21st Century Snakedown")
 
     while True:
         DISPLAYSURF.fill(BLACK)
-        pygame.draw.rect(DISPLAYSURF, WHITE, snakeHead)
-
-        if snakeDirection == UP:
-            snakey += snakeSIZE
-        elif snakeDirection == DOWN:
-            snakey -= snakeSIZE
-        elif snakeDirection == RIGHT:
-            snakex += snakeSIZE
-        elif snakeDirection == LEFT:
-            snakex -= snakeSIZE
+        pygame.draw.rect(DISPLAYSURF, WHITE, snake.head)
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
+        if snake.direction == UP:
+            snake.y += snake.SIZE
+            snake.head.top = snake.y
+        elif snake.direction == DOWN:
+            snake.y -= snake.SIZE
+            snake.head.top = snake.y
+        elif snake.direction == RIGHT:
+            snake.x += snake.SIZE
+            snake.head.left = snake.x
+        elif snake.direction == LEFT:
+            snake.x -= snake.SIZE
+            snake.head.left = snake.x
+
         pygame.display.update()
         fpsClock.tick(FPS)
 
 
 while __name__ == '__main__':
-    main()
+    main(snake)
