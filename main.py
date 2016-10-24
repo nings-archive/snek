3455c32f7fe831195f880bcf8c36e4d38207d159
ningSP4 <ningyuan.sg@gmail.com>
2016-10-24 22:31:03 +0800

    Granted snake with some of the human condition (death).

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ from pygame.locals import *
 import random
 
 FPS = 8
-global SIZE
 SIZE = 20
 WINDOW_WIDTH = 360
 xGrid = WINDOW_WIDTH // SIZE
 WINDOW_HEIGHT = 480
 yGrid = WINDOW_HEIGHT // SIZE
+TOPBORDER = pygame.Rect(0, -1, WINDOW_WIDTH, 1)
+BOTTOMBORDER = pygame.Rect(0, WINDOW_HEIGHT, WINDOW_WIDTH, 1)
+LEFTBORDER = pygame.Rect(-1, 0, 1, WINDOW_HEIGHT)
+RIGHTBORDER = pygame.Rect(WINDOW_WIDTH, 0, 1, WINDOW_HEIGHT)
 WINDOW_RES = (WINDOW_WIDTH, WINDOW_HEIGHT)
 pygame.display.set_caption("21st Century Snakedown")
 fpsClock = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode(WINDOW_RES)
+gameState = True
 # COLOURS  R :  G :  B
 WHITE  = (255, 255, 255)
 GREY   = (100, 100, 100)
@@ -42,6 +46,13 @@ class Tail:
     def draw(self):
         for each in self.history:
             pygame.draw.rect(DISPLAYSURF, WHITE, each)
+
+    def new(self):
+        self.history = [
+                pygame.Rect(WINDOW_WIDTH//2 - 40, WINDOW_HEIGHT//2, SIZE, SIZE),
+                pygame.Rect(WINDOW_WIDTH//2 - 20, WINDOW_HEIGHT//2, SIZE, SIZE)
+                ]
+        self.score = 0
 tail = Tail()
 
 
@@ -50,13 +61,19 @@ class Snake:
     x = WINDOW_WIDTH // 2  # start x
     y = WINDOW_HEIGHT // 2  # start y
     head = pygame.Rect(x, y, SIZE, SIZE)  #pygame Rect Obj
-    collide = False # turns True in loop if collide with border, or tail
     # directional
     direction = RIGHT  # start right
     change = RIGHT
 
     def update(self):
         self.head = pygame.Rect(self.x, self.y, SIZE, SIZE)
+
+    def new(self):
+        self.x = WINDOW_WIDTH // 2
+        self.y = WINDOW_HEIGHT // 2
+        self.update()
+        self.direction = RIGHT
+        self.change = RIGHT
 snake = Snake()
 
 
@@ -73,7 +90,8 @@ food = Food()
 
 
 def game(snake, tail):
-    while True:
+    global gameState
+    while gameState:
         DISPLAYSURF.fill(BLACK)
         pygame.draw.rect(DISPLAYSURF, WHITE, snake.head)
         tail.draw()
@@ -125,13 +143,52 @@ def game(snake, tail):
         while foodblock(snake, tail):
             food.new()
 
+        # lose conditionals
+        headintail, headinwall = False, False
+        for block in tail.history:
+            if block.collidepoint((snake.x, snake.y)):
+                headintail = True
+        headintop = snake.head.top < TOPBORDER.bottom
+        headinbottom = snake.head.bottom > BOTTOMBORDER.top
+        headinleft = snake.head.left < LEFTBORDER.right
+        headinright = snake.head.right > RIGHTBORDER.left
+        headinwall = headintop or headinbottom or headinleft or headinright
+        if headinwall or headintail:
+            gameState = False
+
         pygame.display.update()
         fpsClock.tick(FPS)
 
 
+def lose(snake, tail, food):
+    global gameState
+    while not gameState:
+        DISPLAYSURF.fill(BLACK)
+        pygame.draw.rect(DISPLAYSURF, WHITE, snake.head)
+        tail.draw()
+        pygame.draw.rect(DISPLAYSURF, ORANGE, food.point)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            # newgame
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    gameState = True
+                    snake.new()
+                    tail.new()
+                    food.new()
+
+        pygame.display.update()
+        fpsClock.tick(FPS)
+   
+
 def main(snake, tail):
-    pygame.init()
-    game(snake, tail)
+    while True:
+        pygame.init()
+        game(snake, tail)
+        lose(snake, tail, food)
 
 
 while __name__ == '__main__':
